20ad89d16bd0bc3c9a6540111af205a7eab94d38
Duilio Rubio <111440362+DuRubio@users.noreply.github.com>
2023-10-24 19:25:34 -0300

    antes de pushear

--- controller/ProfileController.php
+++ controller/ProfileController.php
@@ -11,9 +11,11 @@ class ProfileController {
 
     public function show() {
         $datos = [
+            //'usuario' == $this->model->traerUsuario($_SESSION['usuario']['id']),
             'usuario' => $_SESSION['usuario']['usuario'],
             'usuarioPuntos' => $_SESSION['usuario']['puntos'],
-            'nivel' => $_SESSION['usuario']['nivel']
+            'nivel' => $_SESSION['usuario']['nivel'],
+            'user' => $_SESSION['usuario']
         ];
         $this->render->printViewSesion('perfil', $datos);
     }
@@ -25,6 +27,10 @@ class ProfileController {
 
     public function verPerfil() {
         if (isset($_GET['id'])) {
+            if($_GET['id']==$_SESSION['usuario']['id']){
+                header('location:/profile/show');
+            //$this->render->printViewSesion('perfil/show');
+            }
             $idUsuario = $_GET['id'];
 
             if (ctype_digit($idUsuario) && $idUsuario > 0) {
@@ -32,7 +38,7 @@ class ProfileController {
 
                 if ($usuario) {
                     $data = [
-                        'user' => $usuario,
+                        'otroUser' => $usuario,
                         'usuario' => $_SESSION['usuario']['usuario'],
                         'usuarioPuntos' => $_SESSION['usuario']['puntos'],
                         'nivel' => $_SESSION['usuario']['nivel']
@@ -47,4 +53,33 @@ class ProfileController {
             }
         }
 }
+
+    public function modificarUsuario() {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_SESSION['usuario']['id'];
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $anioNacimiento = $_POST['anio_nacimiento'];
+            $pais = $_POST['pais'];
+            $ciudad = $_POST['ciudad'];
+            $mail = $_POST['mail'];
+            $usuario = $_POST['usuario'];
+            $clave = $_POST['clave'];
+
+           $fotoPerfil = $_SESSION['usuario']['foto_perfil']; // Mantén la foto de perfil actual si no se envía una nueva
+
+            if (isset($_FILES["fileInput"]) && $_FILES["fileInput"]["error"] === UPLOAD_ERR_OK) {
+                move_uploaded_file($_FILES["fileInput"]["tmp_name"], "./public/usuarios/" . $_FILES['fileInput']['name']);
+                $fotoPerfil = $_FILES['fileInput']['name'];
+            }
+
+            // se necesita validaciones para modificar el usuario?
+            //$_SESSION['usuario'] = $this->model->traerUsuario($id); // Actualizar datos de usuario en sesión
+            $this->model->modificarUsuario($id,$nombre, $apellido, $anioNacimiento, $pais, $ciudad, $mail, $usuario, $clave, $fotoPerfil);
+
+            header('location:/profile/show');
+        } else {
+            // Manejar solicitud GET si es necesario
+        }
+    }
 }
