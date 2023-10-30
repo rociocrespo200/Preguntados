4683622c1640a43cb9f5e721896cdf1e97fc2c8f
karen-nina <karen_nina4.0@hotmailcom>
2023-10-30 19:41:09 -0300

    sugererir

--- controller/SugerenciasController.php
+++ controller/SugerenciasController.php
@@ -1,6 +1,6 @@
 <?php
 
-class SugerenciasController
+    class SugerenciasController
 {
 
     private $render;
@@ -18,5 +18,37 @@ class SugerenciasController
             'user' => $this->model->traerUsuario($_SESSION['usuario']['id'])
         ];
         $this->render->printViewEditor('sugerencias', $datos);
+    }
+
+    public function mandarSolicitud(){
+        $datos = [
+            'user' => $this->model->traerUsuario($_SESSION['usuario']['id']), 
+            "action" => "sugerencias/enviarSolicitud",
+            "titulo" => "Sugerir Pregunta"
+        ];
+        $this->render->printViewEditor('administrarPregunta', $datos);
+    }
+
+    public function enviarSolicitud(){
+        $pregunta = $_POST['pregunta'];
+        $categoria = $_POST['categoria'];
+        $dificultad = $_POST['dificultad'];
+        $respuestaCorrecta = $_POST['respuestaCorrecta'];
+        $respuestaIncorrecta1 = $_POST['respuestaIncorrecta1'];
+        $respuestaIncorrecta2 = $_POST['respuestaIncorrecta2'];
+        $respuestaIncorrecta3 = $_POST['respuestaIncorrecta3'];
+
+        // Realizar validaciones y procesamiento de datos si es necesario
+
+        // Insertar pregunta en la tabla 'pregunta'
+        $idPregunta = $this->model->agregarPregunta($pregunta, $categoria, $dificultad);
+
+        // Insertar respuestas en la tabla 'respuesta'
+        $this->model->agregarRespuesta($idPregunta, $respuestaCorrecta, 1); // La respuesta correcta tiene el atributo 'correcta' = 1
+        $this->model->agregarRespuesta($idPregunta, $respuestaIncorrecta1, 0);
+        $this->model->agregarRespuesta($idPregunta, $respuestaIncorrecta2, 0);
+        $this->model->agregarRespuesta($idPregunta, $respuestaIncorrecta3, 0);
+
+        // Redirigir a alguna página de éxito o mostrar un mensaje de éxito al usuario
     }
 }
--- model/SugerenciasModel.php
+++ model/SugerenciasModel.php
@@ -1,5 +1,6 @@
-<?php
 
+
+<?php
 class SugerenciasModel
 {
 
@@ -13,5 +14,20 @@ class SugerenciasModel
         return $this->database->query("SELECT * FROM `usuario` WHERE id = $id")[0];
     }
 
+    public function agregarPreguntaSugerida($idUsuario, $pregunta, $categoria, $dificultad, $respuestaCorrecta, $respuestaIncorrecta1, $respuestaIncorrecta2, $respuestaIncorrecta3)
+    {
+        $sql = "INSERT INTO sugerencia (id_usuario, pregunta, id_categoria, id_dificultad, respuestaCorrecta, respuestaIncorrecta1, respuestaIncorrecta2, respuestaIncorrecta3) VALUES ($idUsuario, '$pregunta', $categoria, $dificultad, '$respuestaCorrecta', '$respuestaIncorrecta1', '$respuestaIncorrecta2', '$respuestaIncorrecta3')";
+        $this->database->query($sql);
+    }
 
+    public function agregarRespuestasSugerida($idSugerencia, $respuestaCorrecta, $respuestaIncorrecta1, $respuestaIncorrecta2, $respuestaIncorrecta3)
+    {
+        $sql = "UPDATE sugerencia SET respuestaCorrecta = '$respuestaCorrecta', respuestaIncorrecta1 = '$respuestaIncorrecta1', respuestaIncorrecta2 = '$respuestaIncorrecta2', respuestaIncorrecta3 = '$respuestaIncorrecta3' WHERE id = $idSugerencia";
+        $this->database->query($sql);
+    }
+//consultas, cada q se envie el formulario, enviar preguntas y respuestas 
+//dividir los campos en dos 1 - los que son para crear la tabla pregunta y 2- los que son para crear la respuesta
+// CONSULTA 1 (INSERT Pregunta) - $_POST['DIFICULTAD'] -$_POST['CATGEORIA'] - $_POST['DIFICULTAD']
+// obtener el id de la ultima pregunta agregada
+// CONSULTA 2 (INSERT Respuestas) - $_POST['respuestaCorrecta1'] -$idDeLaPreguntaObtenido- Atributo es corrercta si la pregunta es correcta es =1 si es incorrecta es = 0
 }
